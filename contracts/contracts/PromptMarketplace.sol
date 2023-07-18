8d916ab9a885c20a4556fa498e08055dc168fc65
Rishabh Gupta <rishotics@gmail.com>
2023-07-18 12:02:31 +0200

    new verifier

--- contracts/contracts/PromptMarketplace.sol
+++ contracts/contracts/PromptMarketplace.sol
@@ -1,53 +1,41 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "./verifier.sol";
+
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-interface IVerifier {
-    function verify(
-        uint256[2] memory a,
-        uint256[2][2] memory b,
-        uint256[2] memory c,
-        uint256[2] memory input
-    ) external returns (bool);
-}
+// interface IVerifier {
+//     function verify(
+//         uint256[1] calldata pubInputs,
+//         bytes calldata proof
+//     ) external returns (bool);
+// }
 
-contract PromptMarketplace{/// @notice Explain to an end user what this does
-/// @dev Explain to a developer any extra details
-/// @param Documents a parameter just like in doxygen (must be followed by parameter name){
-
+contract PromptMarketplace{
     uint256 promptCount;
 
     mapping (uint256 => bool) public isProof;
     mapping (address => bytes32[]) public userHashes;
 
-    IVerifier verifier = IVerifier();
-
+    Verifier verifier;
 
     constructor() public {
         promptCount = 0;
-    }
-
-    function mint(
-        bytes calldata aigcData,
-        string calldata uri,
-        bytes calldata proof
-    ) public returns (uint256 tokenId){
-
+        verifier = new Verifier();
     }
 
     function mintPromptImage(
         bytes32 ipfsHash, 
-        uint256[2] memory a,
-        uint256[2][2] memory b,
-        uint256[2] memory c,
-        uint256[2] memory input) 
+        uint256[1] calldata pubInputs,
+        bytes calldata proof
+        ) 
     public payable {
         
-        require(isProof[a[0]] == false, "Prompt already minted");
-        require(verifier.verify(a,b,c,input), "Invalid proof")
-        isProof[a[0]] = true;
+        require(isProof[pubInputs[0]] == false, "Prompt already minted");
+        require(verifier.verify(pubInputs, proof), "Invalid proof");
+        isProof[pubInputs[0]] = true;
         userHashes[msg.sender].push(ipfsHash);
         promptCount++;
     }
@@ -55,7 +43,4 @@ contract PromptMarketplace{/// @notice Explain to an end user what this does
     function getUserHashes(address user) public view returns (bytes32[] memory) {
         return userHashes[user];
     }
-
-
-
 }
